fa3aa8c8ba8d5da5d8c1121175da75a7fb2befca
Nikolai Vovk <dyvniy@yandex.ru>
2024-03-28 00:45:19 +0200

    add libraries, update info

--- release/copy_so.sh
+++ release/copy_so.sh
@@ -1,1 +1,3 @@
 cp /home/nv/Documents/build-paho.mqtt.cpp-Desktop-Debug/src/libpaho-mqttpp3.so.1 /home/nv/Documents/smartiko_test/release/libpaho-mqttpp3.so.1
+
+cp /usr/lib/x86_64-linux-gnu/libpqxx-6.4.so /home/nv/Documents/smartiko_test/release/libpqxx-6.4.so
